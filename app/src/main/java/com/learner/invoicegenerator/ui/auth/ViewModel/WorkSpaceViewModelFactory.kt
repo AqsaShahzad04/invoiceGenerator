e061599b3e78b141b35de93fcecf73fa274e9722
aqsa <aqsa70729@gmail.com>
2026-08-18 16:38:19 +0500

    id

--- app/src/main/java/com/learner/invoicegenerator/ui/auth/ViewModel/WorkSpaceViewModelFactory.kt
+++ app/src/main/java/com/learner/invoicegenerator/ui/auth/ViewModel/WorkSpaceViewModelFactory.kt
@@ -2,10 +2,18 @@ package com.learner.invoicegenerator.ui.auth.ViewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.learner.invoicegenerator.data.local.SessionManager
 import com.learner.invoicegenerator.data.repository.WorkspaceRepository
 
-class WorkSpaceViewModelFactory(private val repository: WorkspaceRepository): ViewModelProvider.Factory {
+class WorkspaceViewModelFactory(
+    private val repository: WorkspaceRepository,
+    private val sessionManager: SessionManager
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return WorkspaceViewModel(repository) as T
+        if (modelClass.isAssignableFrom(WorkspaceViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return WorkspaceViewModel(repository, sessionManager) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
